dd1dceb6fb4fd11921a6563551775a7760447b78
Damian Tarnawsky <damian@ionic.io>
2022-09-14 12:31:15 -0700

    Capacitor 4 dev build for http

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # File Download and Share
 This is a sample Capacitor application that downloads a PDF file then shares it using `@capacitor/filesystem` and `@capacitor/share`.
+
+# cordova-plugin-fileopener2
+
+npm install jetifier
+npx jetifier
+
--- src/app/download.service.ts
+++ src/app/download.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-import { Http, HttpDownloadFileResult, HttpResponse } from '@capacitor-community/http';
 import { Directory, Encoding, Filesystem } from '@capacitor/filesystem';
 
 
@@ -10,23 +9,12 @@ export class DownloadService {
 
   constructor() { }
 
-  public async download(url: string): Promise<string> {
-    const options = {
-      url,
-      filePath: 'document.pdf',
-      fileDirectory: Directory.Data,
-      method: 'GET',
-    };
-    const result: HttpDownloadFileResult = await Http.downloadFile(options);
-    return result.path;
-  }
-
-  public async get(url: string): Promise<any> {
+ public async get(url: string): Promise<any> {
     const options = {
       url
     };
-    const result: HttpResponse = await Http.get(options);
-    return result.data;
+    const response = await fetch(url, { method: 'GET'});
+    return response.blob();
   }
 
   public async write(data: any, directory: Directory): Promise<string> {
