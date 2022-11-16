a1e66805d9340f67a285f9f68e00c3a625a5ce0e
Damian Tarnawsky <damian@ionic.io>
2022-11-16 13:32:12 -0800

    Add file share with multiple files

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
 # File Download and Share
 This is a sample Capacitor application that downloads a PDF file then shares it using `@capacitor/filesystem` and `@capacitor/share`.
 
-# cordova-plugin-fileopener2
+## Notes
 
-npm install jetifier
-npx jetifier
-
+- bug to fix: PDF is downloaded and written to file as UTF-8 using `Blob.text()` which when read is altering some characters in the final file.
+- When saved to `data` folder it relies on the entry `<files-path name="data" path="." />` in `file_paths.xml` which isnt working.
--- src/app/download.service.ts
+++ src/app/download.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Directory, Encoding, Filesystem } from '@capacitor/filesystem';
 
 
+
 @Injectable({
   providedIn: 'root'
 })
@@ -9,24 +10,41 @@ export class DownloadService {
 
   constructor() { }
 
- public async get(url: string): Promise<any> {
-    const options = {
-      url
-    };
+  public async get(url: string): Promise<Blob> {
     console.log(`getting ${url}`);
-    const response = await fetch(url, { method: 'GET'});
+    const response = await fetch(url, { method: 'GET' });
     console.log(`got ${response.statusText}`);
-    return response.blob();
+    return await response.blob();
   }
+
+  public async asBase64(blob: Blob): Promise<any> {
+    return new Promise((resolve) => {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        const b64 = (reader.result as string).replace(/^data:.+;base64,/, '');
+        resolve(b64);
+      };
+      reader.readAsDataURL(blob);
+    });
+  }
+
 
   public async write(data: any, directory: Directory): Promise<string> {
     const result = await Filesystem.writeFile({
-      path: 'mypdf.pdf',
+      path: 'my-pdf.pdf',
       data,
       directory,
       encoding: Encoding.UTF8,
     });
-    console.log('written file');
+    console.log(`Written to ${directory} ${result.uri}`);
     return result.uri;
+  }
+
+  public async read(directory: Directory): Promise<any> {
+    const result = await Filesystem.readFile({
+      path: 'my-pdf.pdf',
+      directory,
+    });
+    return result.data;
   }
 }
